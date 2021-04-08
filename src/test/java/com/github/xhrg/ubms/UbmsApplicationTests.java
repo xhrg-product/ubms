862775cf7d944dcc6a999bde08ec753f714667c1
xhrg <634789257@qq.com>
2021-04-08 14:03:49 +0800

    int project

---
+++ src/test/java/com/github/xhrg/ubms/UbmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.xhrg.ubms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UbmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
