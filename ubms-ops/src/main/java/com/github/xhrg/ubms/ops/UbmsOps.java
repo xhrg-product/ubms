608b3c79de31a798b5a2a27c9b216cf1e7ccd08e
xhrg <634789257@qq.com>
2021-04-08 14:43:43 +0800

    修改项目目录结构

---
+++ ubms-ops/src/main/java/com/github/xhrg/ubms/ops/UbmsOps.java
@@ -0,0 +1,12 @@
+package com.github.xhrg.ubms.ops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UbmsOps {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UbmsOps.class, args);
+    }
+}
---
+++ ubms-server/src/main/java/com/github/xhrg/ubms/server/UbmsServer.java
@@ -0,0 +1,8 @@
+package com.github.xhrg.ubms.server;
+
+public class UbmsServer {
+
+    public static void main(String[] args) {
+
+    }
+}
